460ff88a8602afb93c615c24228a17dac8dffbbd
Jacky <p66074086@gs.ncku.edu.tw>
2022-06-11 08:49:02 +0800

    source conda.sh

--- gdnet-setup.sh
+++ gdnet-setup.sh
@@ -1,8 +1,9 @@
 # export CUDA_HOME=/usr/local/cuda-10.0
 # export CUDA_HOME=/usr/local/cuda-10.1
+. /c/Users/p6607/anaconda3/etc/profile.d/conda.sh
+conda activate py38
 cd GDNet/extensions
 rm -r build
-conda activate py38
 # /opt/conda/bin/python ./setup.py install --user
 python ./setup.py install --user
 read -p "Press any key to continue..."
